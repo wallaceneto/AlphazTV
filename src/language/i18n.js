36bf65e518d8c10db2955734d28df562b837b139
Wallace Neto <wallace.neto@outlook.com>
2025-07-19 21:20:11 -0300

    feat: adding translation library

---
+++ src/language/i18n.js
@@ -0,0 +1,14 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import enUs from './en-us.json'
+import ptBr from './pt-br.json'
+
+export default i18n.use(LanguageDetector).use(initReactI18next).init({
+  compatibilityJSON: 'v3',
+  lng: 'en-US',
+  resources: {
+    'en-US': enUs,
+    'pt-BR': ptBr,
+  },
+});
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import '../src/language/i18n'
+// eslint-disable-next-line no-unused-vars
+import i18next from 'i18next'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
