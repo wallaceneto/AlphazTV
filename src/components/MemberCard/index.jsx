0b863938aa4e9bf04f73111c1a2e58d5fb8ec401
Wallace Neto <wallace.neto@outlook.com>
2025-07-28 10:00:59 -0300

    feat: creating member card component

---
+++ src/components/MemberCard/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './MemberCard.module.css'
+
+export default function MemberCard({ name, memberImage }) {
+  return (
+    <a
+      className={styles.container}
+      onClick={() => {}}
+    >
+      <div className={styles.textContainer}>
+        <h1 className={styles.mainTitle}>
+          THIS IS
+        </h1>
+        <h1 className={styles.nameTitle}>
+          {name}
+        </h1>
+      </div>
+
+      <img src={memberImage} className={styles.image} alt={'Imagem da ' + name} />
+    </a>
+  )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,39 @@
 import React from 'react'
 import { HomepageLayout } from '../../layout'
 import { VideoCarousel } from '../../components/VideoCarousel'
-import classes from './Home.module.css'
+import styles from './Home.module.css'
 
 import mockvideos from '../../mock/mvs_playlist.json'
+import MemberCard from '../../components/MemberCard'
+
+import jurin from '../../assets/members/jurin.jpg'
+import chisa from '../../assets/members/chisa.jpg'
+import hinata from '../../assets/members/hinata.jpg'
+import harvey from '../../assets/members/harvey.jpg'
+import juria from '../../assets/members/juria.jpg'
+import maya from '../../assets/members/maya.jpg'
+import cocona from '../../assets/members/cocona.jpg'
 
 export default function Home() {
 
   return (
     <HomepageLayout>
-      <div className={classes.content}>
-        <div className={classes.firstCarousel}>
+      <div className={styles.content}>
+        <div className={styles.firstCarousel}>
           <VideoCarousel playlist={mockvideos} hideLabel />
         </div>
-        <div className={classes.carousel}>
+        <div className={styles.carousel}>
           <VideoCarousel playlist={mockvideos} />
+        </div>
+
+        <div className={styles.memberCarousel}>
+          <MemberCard name={'Jurin 🦋'} memberImage={jurin} />
+          <MemberCard name={'Chisa ☀️'} memberImage={chisa} />
+          <MemberCard name={'Hinata 💎'} memberImage={hinata} />
+          <MemberCard name={'Harvey 🦄'} memberImage={harvey} />
+          <MemberCard name={'Juria 🐿️'} memberImage={juria} />
+          <MemberCard name={'Maya 🐶'} memberImage={maya} />
+          <MemberCard name={'Cocona 🍄'} memberImage={cocona} />
         </div>
       </div>
     </HomepageLayout>
