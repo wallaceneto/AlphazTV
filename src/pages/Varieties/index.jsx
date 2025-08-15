729a2da7530f91885ea01dc7324ede7952149221
Wallace Neto <wallace.neto@outlook.com>
2025-08-15 16:50:15 -0300

    feat: adding carousel component to varieties page

--- src/pages/Varieties/index.jsx
+++ src/pages/Varieties/index.jsx
@@ -4,20 +4,22 @@ import styles from './Varieties.module.css'
 import VarietyCard from './components/VarietyCard'
 import OriginalSeriesCard from './components/OriginalSeriesCard'
 import { HomepageLayout } from '../../layout'
+import { VideoCarousel } from '../../components/VideoCarousel'
 
 import realityShows from '../../mock/reality_shows.json'
 import originalSeries from '../../mock/original_series.json'
+import mockvideos from '../../mock/mvs_playlist.json'
 
 export default function Varieties() {
   return (
     <HomepageLayout>
       <div className={styles.container}>
-        <div>
-          <h2 className={styles.title}>Reality show</h2>
 
+        <div className={styles.contentDiv}>
+          <h2 className={styles.title}>Reality show</h2>
           <Grid container>
             {realityShows.map((show, index) =>
-              <Grid key={index} size={{ xs: 6, md: 4 }}>
+              <Grid key={index} size={{ xs: 12, md: 4 }}>
                 <VarietyCard
                   name={show.name}
                   description={show.description}
@@ -29,9 +31,8 @@ export default function Varieties() {
           </Grid>
         </div>
 
-        <div>
+        <div className={styles.contentDiv}>
           <h2 className={styles.title}>Séries originais</h2>
-
           <Grid container>
             {originalSeries.map((item, index) =>
               <Grid key={index} size={{ xs: 12 }}>
@@ -44,6 +45,14 @@ export default function Varieties() {
               </Grid>
             )}
           </Grid>
+        </div>
+
+        <div className={styles.contentDiv}>
+          <VideoCarousel playlist={mockvideos} />
+        </div>
+
+        <div className={styles.contentDiv}>
+          <VideoCarousel playlist={mockvideos} />
         </div>
       </div>
     </HomepageLayout>
