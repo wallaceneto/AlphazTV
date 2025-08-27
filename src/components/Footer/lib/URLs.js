a42ccdb2310a9f11e03bce1a838b2b02e2ca007c
JaySant <jaysant15@gmail.com>
2025-08-27 11:58:36 -0300

    feat: component about

--- src/components/Footer/lib/URLs.js
+++ src/components/Footer/lib/URLs.js
@@ -2,8 +2,12 @@ const URLs = {
   xgYoutube: "https://www.youtube.com/channel/UC12HMtO5MYph9dCZZ7yygng",
   xgInstagram: "https://www.instagram.com/xgofficial",
   xgTwitter: "https://x.com/xgofficial_",
+  xgSpotify:
+    "https://open.spotify.com/artist/0LOK81e9H5lr61HlGGHqwA?si=HUPsPBmpRISqVCSixxeTCg",
+  xgAppleMusic: "https://music.apple.com/br/artist/xg/1609409493",
+  xgSite: "https://xgalx.com/en/xg",
   contactTwitter: "https://x.com/xgofficial_",
-  contactBluesky: "https://bsky.app/profile/wallaceneto.bsky.social"
-}
+  contactBluesky: "https://bsky.app/profile/wallaceneto.bsky.social",
+};
 
-export { URLs }
+export { URLs };
