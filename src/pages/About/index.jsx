fa0818d87a10c6a3c2c6746e53d94c29618b751a
Wallace Neto <wallace.neto@outlook.com>
2025-09-02 11:57:15 -0300

    feat: appling gallery and translation to about page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -5,11 +5,15 @@ import {
   faApple,
 } from "@fortawesome/free-brands-svg-icons";
 import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { useTranslation } from 'react-i18next';
 import styles from "./About.module.css";
 import { HomepageLayout } from "../../layout";
-import { about } from "../../mock/about.json";
 import { URLs } from "../../global/lib/URLs";
 import IconButton from "../../components/IconButton";
+import PhotoGallery from "../../components/PhotoGallery";
+
+import { about } from "../../mock/about.json";
+import groupGallery from '../../mock/group_gallery.json';
 
 const socialLinks = [
   { icon: faGlobe, url: URLs.xgSite, label: "Site" },
@@ -34,24 +38,44 @@ function SocialLinks() {
 }
 
 export default function About() {
+  const { t } = useTranslation();
+
   return (
     <HomepageLayout>
-      {about.map((section, index) => (
-        <div key={section.title} className={styles.container_about}>
-          <img
-            src={section.image}
-            alt={section.title}
-            className={styles.image}
-          />
-          <div className={styles.content}>
-            <h1 className={styles.title}>{section.title}</h1>
-            <p className={styles.description}>{section.description}</p>
-            <div className={styles.container_socialLinks}>
-              {index === 0 && <SocialLinks />}
-            </div>
+      <div key={about[0].title} className={styles.container_about}>
+        <img
+          src={about[0].image}
+          alt={about[0].title}
+          className={styles.image}
+        />
+        <div className={styles.content}>
+          <h1 className={styles.title}>{t(`AboutPage.${about[0].title}`)}</h1>
+          <p className={styles.description}>{t(`AboutPage.${about[0].description}`)}</p>
+          <div className={styles.container_socialLinks}>
+            <SocialLinks />
           </div>
         </div>
-      ))}
+      </div>
+
+      <div className={styles.galleryContainer}>
+        <h2 className={styles.title}>{t('Gallery')}</h2>
+        <PhotoGallery
+          galleryPath={groupGallery.galleryPath}
+          galleryLength={groupGallery.galleryLength}
+        />
+      </div>
+
+      <div key={about[1].title} className={styles.container_about}>
+        <img
+          src={about[1].image}
+          alt={about[1].title}
+          className={styles.image}
+        />
+        <div className={styles.content}>
+          <h1 className={styles.title}>{t(`AboutPage.${about[1].title}`)}</h1>
+          <p className={styles.description}>{t(`AboutPage.${about[1].description}`)}</p>
+        </div>
+      </div>
     </HomepageLayout>
   );
 }
