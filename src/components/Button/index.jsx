11c63de44b654eb4cfdba49aa169879c9c1b2511
Wallace Neto <wallace.neto@outlook.com>
2025-09-02 10:15:09 -0300

    feat: adding arrow buttons to gallery component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from './Button.module.css'
 
-const Button = ({ children, onClick }) => {
+const Button = ({ children, onClick, disabled }) => {
   return (
     <button
-      className={[styles.container]}
+      className={styles.container}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
--- src/components/PhotoGallery/index.jsx
+++ src/components/PhotoGallery/index.jsx
@@ -1,12 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons"
 import styles from './PhotoGallery.module.css'
 import Button from '../Button'
 
 const PhotoGallery = ({ galleryPath, galleryLength }) => {
   const [loading, setLoading] = useState(true);
   const [gallery, setGallery] = useState([]);
-  const [emblaRef] = useEmblaCarousel({ align: 'start' });
+  const [emblaRef, emblaApi] = useEmblaCarousel({ align: 'start' });
+  const [prevBtnDisabled, setPrevBtnDisabled] = useState(true)
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(true)
+
+  const onNextButton = useCallback(() => {
+    if (!emblaApi) return
+    emblaApi.scrollNext()
+  }, [emblaApi])
+
+  const onPreviousButton = useCallback(() => {
+    if (!emblaApi) return
+    emblaApi.scrollPrev()
+  }, [emblaApi])
+
+  const onSelect = useCallback((emblaApi) => {
+    setPrevBtnDisabled(!emblaApi.canScrollPrev())
+    setNextBtnDisabled(!emblaApi.canScrollNext())
+  }, [])
+
+  useEffect(() => {
+    if (!emblaApi) return
+
+    onSelect(emblaApi)
+    emblaApi.on('reInit', onSelect).on('select', onSelect)
+  }, [emblaApi, onSelect])
 
   useEffect(() => {
     const photos = [];
@@ -36,6 +62,25 @@ const PhotoGallery = ({ galleryPath, galleryLength }) => {
                   </a>
                 </div>
               )}
+            </div>
+
+            <div className={styles.slideButtonContainer}>
+              <Button onClick={() => onPreviousButton()} disabled={prevBtnDisabled}>
+                <div className={styles.slideButton}>
+                  <FontAwesomeIcon
+                    icon={faChevronLeft}
+                    className={styles.slideButtonIcon}
+                  />
+                </div>
+              </Button>
+              <Button onClick={() => onNextButton()} disabled={nextBtnDisabled}>
+                <div className={styles.slideButton}>
+                  <FontAwesomeIcon
+                    icon={faChevronRight}
+                    className={styles.slideButtonIcon}
+                  />
+                </div>
+              </Button>
             </div>
           </div>
       }
