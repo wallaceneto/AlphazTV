3d2902190150e7af9b0fa97801ea36e1376c0584
Wallace Neto <wallace.neto@outlook.com>
2025-09-04 10:04:24 -0300

    feat: changing top banner images

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,26 +1,42 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import styles from './Banner.module.css'
 import Header from '../Header'
 import { ThemeContext } from '../../contexts'
 
-import darkBanner from '../../assets/dark_banner.png'
-import lightBanner from '../../assets/light_banner.png'
+import darkBanner from '../../assets/dark_logo_banner.png'
+import lightBanner from '../../assets/light_logo_banner.png'
+import mobileBanner from '../../assets/mobile_logo_banner.png'
 
 const Banner = () => {
   const { theme } = useContext(ThemeContext);
+  const [mobileMode, setMobileMode] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth < 767) {
+        setMobileMode(true);
+      } else {
+        setMobileMode(false);
+      }
+    }
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+
+  }, []);
 
   return (
     <div className={styles.bannerContainer}>
       <Header />
       <img
-        src={theme === 'dark' ? darkBanner : lightBanner}
+        src={
+          mobileMode ? mobileBanner :
+            theme === 'dark' ? darkBanner : lightBanner
+        }
         className={styles.image}
         alt='imagem do grupo XG'
       />
-
-      <div className={styles.overlay} />
-
-      {/* <h1 className={styles.title}>Alphaz TV+</h1> */}
     </div>
   )
 }
